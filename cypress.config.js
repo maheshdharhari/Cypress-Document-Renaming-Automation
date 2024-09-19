8c86d748e5f4df2ea3498bb8fc63fafac4ecbd3c
Mahesh Kumar Yadav <contact@maheshyadav.com.np>
2024-09-19 11:17:08 +0545

    added a imaginary url

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
 	   redirectionLimit: 300, 
-	  baseUrl:'https://maheshyadav.com.np',
+	  baseUrl:'https://imaginary-url.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/ContentServer/renameContainer.cy.js
+++ cypress/e2e/ContentServer/renameContainer.cy.js
@@ -5,7 +5,7 @@ describe("Renaming a container in Contant Server", () => {
     it("Should be able to submit a successful submission via user login page", () =>{
         //cypress code
         for (let i = 0; i < 300; i++) {
-        cy.visit("https://cstest.nguk.cloud.opentext.com/livelink/livelink.exe?func=ll&objId=3152555&objAction=properties&nexturl=%2Flivelink%2Flivelink%2Eexe%3Ffunc%3Dll%26objId%3D3152555%26objAction%3Dbrowse%26viewType%3D1");
+        cy.visit("https://imaginary-url.com/livelink/livelink.exe?func=ll&objId=3152555&objAction=properties&nexturl=%2Flivelink%2Flivelink%2Eexe%3Ffunc%3Dll%26objId%3D3152555%26objAction%3Dbrowse%26viewType%3D1");
         // Clear the existing document name and type a new one
         cy.get('#docName').clear().type(i);
         // Click the save button
